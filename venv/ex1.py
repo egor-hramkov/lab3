6bf9429af71223a4ae557e0a48f4fcb259cd31e4
egor-hramkov <egor_kuplinov@mail.ru>
2022-03-23 16:11:04 +0400

    init

--- venv/ex1.py
+++ venv/ex1.py
@@ -1,4 +1,6 @@
 import requests
+import typing
+import json
 
 host = 'https://geocode-maps.yandex.ru/1.x/'
 
@@ -7,18 +9,44 @@ params_query = {
     'geocode': '',
     'format': 'json',
 }
+params_query_2 = {
+    'll': '',
+    'spn': '',
+    'l': 'map',
+}
 
 city = input()
 params_query['geocode'] = city
 
 resp = requests.get(host, params=params_query)
-print(resp.status_code)
-print(resp.json())
-print(resp.reason)
-print(resp.url)
 
-with open('data.json', mode='w', encoding='utf8') as f:
-    f.write(resp.text)
+if resp.status_code == 200:
+    with open('data.json', mode='w', encoding='utf8') as f:
+        f.write(resp.text)
 
-atitude = ""
-latitude = ""
+    data = resp.json()['response']["GeoObjectCollection"]["featureMember"]
+    if data != []:
+        coordinates = data[0]["GeoObject"]["Point"]["pos"]  # type: str
+        atitude = tuple(coordinates.split(" "))
+        print(atitude)
+
+        host = 'https://static-maps.yandex.ru/1.x/'
+
+        if(len(city.split()) == 1):
+            params_query_2['spn'] = '0.1646,0.0619'
+        else:
+            params_query_2['spn'] = '0.0100,0.00100'
+
+        params_query_2['ll'] = atitude[0] + ','
+        params_query_2['ll'] += atitude[1]
+        resp = requests.get(host, params=params_query_2)
+        if resp.status_code == 200:
+            with open('map.png', mode='wb') as f:
+                f.write(resp.content)
+
+
+
+    else:
+        print("К сожалению, по вашему запросу ничего не нашлось!!!")
+else:
+    print("Ошибка: ", resp.status_code)
