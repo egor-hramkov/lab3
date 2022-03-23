1cc96e580439a1ffd10dac6b31b19626d0bfea3e
egor-hramkov <egor_kuplinov@mail.ru>
2022-03-23 21:48:59 +0400

    init

---
+++ venv/ex2.py
@@ -0,0 +1,57 @@
+from flask import Flask, url_for
+from werkzeug.utils import redirect
+import requests
+app = Flask(__name__)
+
+host = 'https://geocode-maps.yandex.ru/1.x/'
+host2 = 'https://static-maps.yandex.ru/1.x/'
+
+
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('city_map'))
+
+@app.route('/city_map/<city>/')
+@app.route('/city_map/')
+def city_map(city='Ульяновск'):
+
+    params_query = {
+        'apikey': '46b49608-1968-44d4-b854-b9fbfcc115ae',
+        'geocode': '',
+        'format': 'json',
+    }
+    params_query_2 = {
+        'll': '',
+        'spn': '0.1646,0.0619',
+        'l': 'map',
+    }
+
+    params_query['geocode'] = city
+    resp = requests.get(host, params=params_query)
+    with open('data2.json', mode='w', encoding='utf8') as f:
+        f.write(resp.text)
+
+    data = resp.json()['response']["GeoObjectCollection"]["featureMember"]
+    if data != []:
+        coordinates = data[0]["GeoObject"]["Point"]["pos"]  # type: str
+        atitude = tuple(coordinates.split(" "))
+    else:
+        pass
+
+    params_query_2['ll'] = atitude[0] + ','
+    params_query_2['ll'] += atitude[1]
+    resp = requests.get(host2, params=params_query_2)
+    with open('map.png', mode='wb') as f:
+        f.write(resp.content)
+
+
+    return f'''
+    <h1>ВАШ ГОРОД {city}</h1>
+'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
